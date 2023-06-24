0f01d90194257cb0b6c7e39b68cd74613edd0e57
Andres Rodriguez <andresrodriguezesteban@gmail.com>
2023-06-24 06:20:54 -0300

    added login function

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const dotenv = require('dotenv')
 const express = require('express')
+const jwt = require('jsonwebtoken');
 const app = express()
 const port = 3000
 
@@ -8,9 +9,25 @@ dotenv.config()
 const apiKEY = process.env.API_KEY_VALUE
 const apiURL = process.env.API_BASE_URL
 
-app.get("/movie", function(req, res) {
+app.post('/login', function(req, res) {
+    if (!req.query.user || !req.query.password) {res.status(400).json({error: 'Bad Request'}); return}
+
+    const auth = req.query
+
+    if (auth.user == process.env.AUTH_USER && auth.password == process.env.AUTH_PASSWORD) {
+        res.json({
+            token: generateAccessToken()
+        })
+    } else {
+        res.status(401).json({error: 'Invalid Credentials'})
+    }
+})
+
+app.get('/movie', function(req, res) {
+    if (Object.keys(req.query).length == 0) {res.status(400).json({error: 'Bad Request'}); return}
+
     fetch(`${apiURL}/?apikey=${apiKEY}${formatParams(req.query)}`, {
-        method: "GET"
+        method: 'GET'
     })
     .then(resp => resp.json())
     .then(data => {
@@ -19,7 +36,7 @@ app.get("/movie", function(req, res) {
 })
 
 app.listen(port, () => {
-    console.log(`Movies Backend Running...`)
+    console.log(`IMDb Proxy Middleware Running...`)
 })
 
 function formatParams(params) {
@@ -32,4 +49,8 @@ function formatParams(params) {
     }
 
     return string
+}
+
+function generateAccessToken () {
+    return jwt.sign({user: process.env.AUTH_USER}, process.env.JWT_TOKEN, {expiresIn: '20m'});
 }
