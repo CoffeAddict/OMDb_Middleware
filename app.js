4c6af900fcd58700b312b086532bf0bd7fc75751
Andres Rodriguez <andresrodriguezesteban@gmail.com>
2023-06-27 06:23:41 -0300

    added comments + improved URLSearchParams

--- app.js
+++ app.js
@@ -4,22 +4,25 @@ const jwt = require('jsonwebtoken');
 const cors = require('cors');
 const app = express()
 
-
 dotenv.config()
 
+// Import environment variables
 const port = process.env.PORT || 3000
 const apiKEY = process.env.API_KEY_VALUE
 const apiURL = process.env.API_BASE_URL
 
+// Config Cross Origin Resource Sharing
 app.use(cors({
     origin: process.env.APP_URL
 }))
 
 app.post('/login', function(req, res) {
+    // Verifiy that request has all paremeters
     if (!req.query.user || !req.query.password) {returnError(res, 400, 'Bad Request'); return}
 
     const auth = req.query
 
+    // Compare credentials with dummy login
     if (auth.user == process.env.AUTH_USER && auth.password == process.env.AUTH_PASSWORD) {
         res.json({
             token: generateAccessToken()
@@ -30,11 +33,20 @@ app.post('/login', function(req, res) {
 })
 
 app.get('/movie', function(req, res) {
+    // Check token's validation
     if (!checkAccessToken(req)) {returnError(res, 403, 'Unauthorized'); return}
 
+    // Check request has required parameters
     if (Object.keys(req.query).length == 0) {returnError(res, 400, 'Bad Request'); return}
 
-    fetch(`${apiURL}/?apikey=${apiKEY}${formatParams(req.query)}`, {
+    // Delete token for local validation
+    delete req.query.token
+
+    // Create string with parameters and add API Token
+    const params = new URLSearchParams(req.query)
+    params.append('apikey', apiKEY)
+
+    fetch(`${apiURL}/?${params}`, {
         method: 'GET'
     })
     .then(resp => resp.json())
@@ -43,10 +55,12 @@ app.get('/movie', function(req, res) {
     })
 })
 
+// Send non existent GET request to 404
 app.get('*', function(req, res){
     returnError(res, 404, 'Not Found')
 })
 
+// Send non existent POST request to 404
 app.post('*', function(req, res){
     returnError(res, 404, 'Not Found')
 })
@@ -54,18 +68,6 @@ app.post('*', function(req, res){
 app.listen(port, () => {
     console.log(`IMDb Proxy Middleware Running...`)
 })
-
-function formatParams(params) {
-    if (!params) return ''
-
-    let string = ''
-
-    for (property in params) {
-        string += `&${property}=${params[property]}`
-    }
-
-    return string
-}
 
 function generateAccessToken () {
     return jwt.sign({user: process.env.AUTH_USER}, process.env.JWT_TOKEN, {expiresIn: '20m'});
